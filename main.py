924493ae02b999e6f0babd5c4e8e5cb0891257fd
w3schools-test <test@w3schools.com>
2021-12-29 14:11:53 -0600

    Final for Grade

--- main.py
+++ main.py
@@ -1,131 +1,121 @@
 import time
 import json
-import numpy
 import names
 import random
-from faker import Faker
 from datetime import datetime
 from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
+import ast
+import apache_beam as beam
+import apache_beam.io.gcp.bigquery
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.io.gcp.internal.clients import bigquery
 
 # pub/sub values
 PROJECT_ID = "york-cdf-start"
-TOPIC = "dataflow-project-orders"
+TOPIC = "sh_test_topic"
+SUBSCRIPTION_ID = "projects/york-cdf-start/subscriptions/sh_test_topic-sub" # my subscription to my topic
+options = beam.options.pipeline_options.PipelineOptions()
+standard_options = options.view_as(beam.options.pipeline_options.StandardOptions)
+standard_options.streaming = True
 
-# range values
-order_frequency_range = {'min': 1, 'max': 5, 'step': 1}
-product_count_range = {'min': 1, 'max': 5, 'step': 1}
-product_price_range = {'min': 50, 'max': 5000, 'step': 0.01}
-cost_tax_range = {'min': 5, 'max': 50, 'step': 0.01}
-cost_shipping_range = {'min': 5, 'max': 50, 'step': 0.01}
+#called in main to create a dictionary from messages in topic subscription
+class Dictionary(beam.DoFn):
+    def process(self, element):
+#        print(element)
+        json_string = element.decode() # initialize variable/ apply 'decode' method to reformat bytestring element to json string
+#        print(json_string)
 
-# init
-fake = Faker()
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(PROJECT_ID, TOPIC)
+        dictionary = json.loads(json_string) #initialize variable/ apply 'loads' method to reformat json to dictionary
+#        print(dictionary)
+        yield dictionary #yields the objects to the pcollection, (names don't matter)
 
+class SplitDictionary(beam.DoFn): #called in main
+    def process(self, element):
 
-def generate_order():
-    customer_name = names.get_full_name()
-    customer_time = datetime.now().strftime('%d/%b/%Y:%H:%M:%S')
-    customer_ip = fake.ipv4()
+        output = {} #initialize dictionary to hold data after processed
 
-    order_id = fake.postcode()
-    order_currency = random.choice(["USD", "EUR", "GBP"])
-    order_address = fake.address().replace('\n', ', ')
-    order_items = generate_customer_cart()
+        # address transformation, split string into list to access and continue transformations
+        address_list = element["order_address"].split(", ")
+        # check for list length after split, 3 = regular address, <3 = skip address
+        if len(address_list) == 3:
+            #split list items to access required parts and store in a variable
+            order_building_number = int(address_list[0].split(" ", 1)[0])
+            order_street_name = address_list[0].split(" ", 1)[1]
+            order_city = address_list[1]
+            order_state_code = address_list[2].split(" ")[0]
+            order_zip_code = int(address_list[2].split(" ")[1])
 
-    cost_shipping = round(
-        random.choice(
-            numpy.arange(
-                cost_shipping_range['min'],
-                cost_shipping_range['max'],
-                cost_shipping_range['step']
-            )
-        ),
-        2
-    )
-    cost_tax = round(
-        random.choice(
-            numpy.arange(
-                cost_tax_range['min'],
-                cost_tax_range['max'],
-                cost_tax_range['step']
-            )
-        ),
-        2
-    )
+            addr = {}  #will hold address items in a dictionary
+            #populate the addr dictionary with the required data in variables
+            addr["order_building_number"] = order_building_number
+            addr["order_street_name"] = order_street_name
+            addr["order_city"] = order_city
+            addr["order_state_code"] = order_state_code
+            addr["order_zip_code"] = order_zip_code
+            output["order_address"] = addr #add addr dictonary to output dictionary
+        else:
+            pass #skips over split string if resulting list != 3 items
 
-    return {
-        'customer_name': customer_name,
-        'customer_time': customer_time,
-        'customer_ip': customer_ip,
+        output["order_id"] = element["order_id"] # populates the dictionary with key:values
+        output["customer_first_name"] = element["customer_name"].split(' ')[0] # split customer_name returns a list, choose the index
+        output["customer_last_name"] = element["customer_name"].split(' ')[1]  # split customer_name returns a list, choose the index
+        output["customer_ip"] = element["customer_ip"] # populates the dictionary with key:values
+        output["order_currency"] = element["order_currency"] #use key to accces value, store it in th output dictionary
 
-        'order_id': order_id,
-        'order_currency': order_currency,
-        'order_address': order_address,
-        'order_items': order_items,
+        item_price = [] # initiate list to hold each price
+        for order_item in element['order_items']: #use index key to get to index value in the dictionary
+            item_price.append(order_item['price']) #append price value to list
+        shipping = element['cost_shipping'] #
+        tax = element['cost_tax']
 
-        'cost_shipping': cost_shipping,
-        'cost_tax': cost_tax
-    }
+        # calcuate a cost_total field from the cost_shipping, cost_tax and each price field in the order_items list
+        cost_total = (round(shipping + tax + sum(item_price), 2)) #variable holds value of shipping + tax plus sum() applied to list
+        output["cost_total"] = cost_total #add field and value into dictionary
+        #print(cost_total)
+        print(output)
+        yield output #stores the processed values/objects back into the "output" variable objects, Pcollection
 
-
-def generate_customer_cart():
-    product_list = []
-    product_count = random.choice(
-        range(
-            product_count_range['min'],
-            product_count_range['max'],
-            product_count_range['step']
-        )
-    )
-
-    # generate customer cart
-    for i in range(product_count):
-        product_list.append({
-            'id': fake.postcode(),
-            'name': names.get_last_name(),
-            'price': round(
-                random.choice(
-                    numpy.arange(
-                        product_price_range['min'],
-                        product_price_range['max'],
-                        product_price_range['step']
-                    )
-                ),
-                2
-            )
-        })
-
-    return product_list
-
-
-def publish(publisher, topic, message):
-    data = json.dumps(message).encode('utf-8')
-    return publisher.publish(topic_path, data=data)
-
-
-def callback(message_future):
-    # When timeout is unspecified, the exception method waits indefinitely.
-    if message_future.exception(timeout=30):
-        print('Publishing message on {} threw an Exception {}.'.format(message_future.exception()))
-    else:
-        print(message_future.result())
-
+#class CurrencySplit(beam.DoFn): #called in main
+#    def process(self, element):
 
 if __name__ == '__main__':
-    while True:
-        order = generate_order()
-        message_future = publish(publisher, topic_path, order)
-        message_future.add_done_callback(callback)
+    PROJECT_ID = "york-cdf-start"
 
-        timeout_seconds = random.choice(
-            range(
-                order_frequency_range['min'],
-                order_frequency_range['max'],
-                order_frequency_range['step']
-            )
-        )
+    TABLE_SCHEMA = {
+        'fields': [
+            {'name': 'order_id', 'type': 'INTEGER', 'mode': 'NULLABlE'},
+            {'name': 'order_address', 'type': 'RECORD', 'mode': 'REPEATED', 'fields': [
+                {'name': 'order_building_number', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+                {'name': 'order_street_name', 'type': 'STRING', 'mode': 'NULLABLE'},
+                {'name': 'order_city', 'type': 'STRING', 'mode': 'NULLABLE'},
+                {'name': 'order_state_code', 'type': 'STRING', 'mode': 'NULLABLE'},
+                {'name': 'order_zip_code', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+            ]},
+            {'name': 'customer_first_name', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'customer_last_name', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'customer_ip', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'cost_total', 'type': 'FLOAT', 'mode': 'NULLABLE'},
+        ]
+    }
 
-        print(order)
-        time.sleep(timeout_seconds)
+    #to apply a write transformation you need: destination table name/dest.table create disposition/dest.table write disposition
+    #BigQueryDisposition.CREATE_IF_NEEDED
+
+    table_spec = bigquery.TableReference(
+        PROJECT_ID="york-cdf-start",
+        datasetId="SH_dataflow_project",
+        tableID="test_table"
+    )
+
+    #Beam driver program must create a Pipeline instance and configure some options to pass to the Pipeline object
+    with beam.Pipeline(options=options) as p:
+        #use PubSub in beam pipeline to access topic subscription messages. sub_ID already set to subscription path
+        pcoll = p | 'ReadFromPub' >> beam.io.ReadFromPubSub(subscription=SUBSCRIPTION_ID)
+        #variable initialized to call class and then hold new value of class output "yield"
+        dictionary = pcoll | 'pcoll into dictionary' >> beam.ParDo(Dictionary())
+        split_data = dictionary | 'dict into split fields' >> beam.ParDo(SplitDictionary())
+ #       currency_split = split_data | 'split_data gets sorted by currency' >> beam.ParDo(CurrencySplit())
